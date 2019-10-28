df7abc5628a9784890e7b01234a459f9b6526d77
ChuckT95 <cftaylor7128@eagle.fgcu.edu>
2019-10-28 17:17:32 -0400

    updated for localhost

--- Client.py
+++ Client.py
@@ -6,7 +6,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     # here, the client connects to the server application, at the values in that application
     # the server should be running already.
     # run this from a separate terminal for testing
-    s.connect(('127.0.0.1', 56821))
+    s.connect(('localhost', 56821))
     # sends the input. remember the maximum byte size for testing, as this may cause problems during testing
     # all that actually needs to be done here is to send some form of signal,
     # so it may be enough to even further simplify this file by simply using connect
--- Server.py
+++ Server.py
@@ -6,7 +6,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     # Step Two: bind the socket to the correct IP_Address and port number.
     # when the Pi is set up, these arguments, seen in bind(), will need to be changed
     # Current values are common for echo servers, seen in class
-    s.bind(('127.0.0.1', 56821))
+    s.bind(('localhost', 56821))
 
     # Step 3, begin listening for a connection from the client
     # the argument of 0 signifies the "backlog" of connections which can exist.
@@ -25,13 +25,14 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         print('Connected by', address)
         # Here, before the infinite loop, would be the place to run the script/subprocess of the arm
         # this can be done simply by importing "subprocess" and adding the line
-        #subprocess.call("./a.out")
+        # subprocess.call("./a.out")
 
         while True:
             # this is just some basic example code for an echo server like we saw in class.
             # 1024 is the number of bytes that can be received. this is the highest
             # value within the recommended limit of the documentation
             data = connection.recv(4096)
+            # subprocess.call("./a.out")
             ###take commands
             # if there is some crazy problem with the input, it will break the loop
             if not data:
